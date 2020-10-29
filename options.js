4d7ef9e119031045fa8037901c133ce4ada9b9a2
keenanbruni <>
2020-10-29 11:20:30 -0700

    consolidated options page

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 // Renders saved username in options 
 const renderUserName = () => {
     chrome.storage.sync.get("userName", (obj) => {
-        document.querySelector("#set-name > input").value = obj.userName
+        document.querySelector("#setName.input").value = obj.userName
     })
 }
 
@@ -10,7 +10,7 @@ renderUserName()
 // Renders saved # of cost days in options
 const renderCostDays = () => {
     chrome.storage.sync.get("costDays", (obj) => {
-        document.querySelector("#set-costs > input").value = obj.costDays
+        document.querySelector("#setCosts.input").value = obj.costDays
     })
 }
 
@@ -19,38 +19,57 @@ renderCostDays()
 // Renders saved company name in options
 const renderCompanyName = () => {
     chrome.storage.sync.get("companyName", (obj) => {
-        document.querySelector("#set-companyName > input").value = obj.companyName
+        document.querySelector("#setCompanyName.input").value = obj.companyName
     })
 }
 
 renderCompanyName()
 
-// Username form
-const nameForm = document.getElementById('set-name')
+// // Username form
+// const nameForm = document.getElementById('set-name')
 
-nameForm.addEventListener('submit', (e) => {
+// nameForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const userName = e.target["0"].value
+//     chrome.storage.sync.set({'userName' : userName}, console.log('Username Set!'))
+//     renderUserName()
+// })
+
+// // Costs form
+// const costsForm = document.getElementById('set-costs')
+
+// costsForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const costDays = e.target["0"].value;
+//     chrome.storage.sync.set({'costDays' : costDays}, console.log('Cost Days Set!'))
+//         renderCostDays()
+// })
+
+// // Company form
+// const companyForm = document.getElementById('set-companyName')
+
+// companyForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const companyName = e.target["0"].value
+//     chrome.storage.sync.set({'companyName' : companyName}, console.log('Company name set!'))
+//     renderCompanyName()
+// })
+
+// Options Form
+const optionsForm = document.getElementById('options-form')
+
+optionsForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const userName = e.target["0"].value
     chrome.storage.sync.set({'userName' : userName}, console.log('Username Set!'))
     renderUserName()
-})
 
-// Costs form
-const costsForm = document.getElementById('set-costs')
-
-costsForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const costDays = e.target["0"].value;
+    const costDays = e.target["1"].value;
     chrome.storage.sync.set({'costDays' : costDays}, console.log('Cost Days Set!'))
-        renderCostDays()
-})
+    renderCostDays()
 
-// Company form
-const companyForm = document.getElementById('set-companyName')
-
-companyForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const companyName = e.target["0"].value
+    const companyName = e.target["2"].value
     chrome.storage.sync.set({'companyName' : companyName}, console.log('Company name set!'))
     renderCompanyName()
 })

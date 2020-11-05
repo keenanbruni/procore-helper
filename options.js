ea8726025e0d08f8ddde0df12273098565a9b5c8
keenanbruni <>
2020-11-05 08:57:28 -0800

    changed button colors

--- options.js
+++ options.js
@@ -25,36 +25,6 @@ const renderCompanyName = () => {
 
 renderCompanyName()
 
-// // Username form
-// const nameForm = document.getElementById('set-name')
-
-// nameForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const userName = e.target["0"].value
-//     chrome.storage.sync.set({'userName' : userName}, console.log('Username Set!'))
-//     renderUserName()
-// })
-
-// // Costs form
-// const costsForm = document.getElementById('set-costs')
-
-// costsForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const costDays = e.target["0"].value;
-//     chrome.storage.sync.set({'costDays' : costDays}, console.log('Cost Days Set!'))
-//         renderCostDays()
-// })
-
-// // Company form
-// const companyForm = document.getElementById('set-companyName')
-
-// companyForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const companyName = e.target["0"].value
-//     chrome.storage.sync.set({'companyName' : companyName}, console.log('Company name set!'))
-//     renderCompanyName()
-// })
-
 // Options Form
 const optionsForm = document.getElementById('options-form')
 
